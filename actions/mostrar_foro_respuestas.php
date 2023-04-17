6ccc45d8a9ded53dbfdd45fd101d3f49280fb17d
Juan Vicente Brotons Martinez <jvbrotons@hotmail.es>
2023-04-17 12:40:57 +0200

    añadir respuestas al foro

--- actions/mostrar_foro_respuestas.php
+++ actions/mostrar_foro_respuestas.php
@@ -5,7 +5,7 @@ include_once '../utiles/funciones.php';
 try {
     $conexion = conexionBD();
     $id       = $_POST['id'];
-    $sql      = "SELECT * FROM respuestas WHERE for_id=" . $id;
+    $sql      = "SELECT consulta1.respuesta respuesta_principal, consulta1.fecha, consulta1.usuario, consulta2.respuesta respuesta_secundaria FROM (SELECT res.id, res.respuesta, res.foro_id, res.fecha, usu.usuario FROM respuestas res INNER JOIN usuarios usu ON res.usuario_id=usu.ID) consulta1 LEFT JOIN (SELECT respu.id, estas.respuesta FROM respuestas respu INNER JOIN respuestas estas ON respu.id=estas.respuesta_id) consulta2 ON consulta1.id=consulta2.id WHERE consulta1.foro_id=" . $id . " ORDER BY consulta1.id";
 
     $resul = $conexion->query($sql) or die(print($conexion->errorInfo()));
 
@@ -13,9 +13,10 @@ try {
 
     while ($row = $resul->fetch(PDO::FETCH_OBJ)) {
         $datos [] = [
-            'id'       => $row->foro,
-            'pregunta' => $row->pregunta,
-            'fechaPre' => $row->fecha
+            'principal'  => $row->respuesta_principal,
+            'fecha'      => $row->fecha,
+            'usuario'    => $row->usuario,
+            'secundaria' => $row->respuesta_secundaria
         ];
     }
 
--- paginas/foro.php
+++ paginas/foro.php
@@ -12,7 +12,6 @@
 
     const contPre = document.getElementById("contenedor-preguntas");
     const error = document.querySelector(".noPreguntas");
-
     const preguntas = async () => {
         try {
             const response = await fetch('./actions/mostrar_foro.php');
@@ -27,7 +26,6 @@
             console.log(error.message);
         }
     };
-
     function mostrarPreguntas() {
         preguntas()
                 .then(datos => {
@@ -49,12 +47,9 @@
                             pIn2Pregunta.innerText = item.pregunta;
                             divIn2.appendChild(pIn2Pregunta);
                             let divIn3 = document.createElement("div");
-                            let pIn3Fecha1 = document.createElement("p");
-                            let pIn3Fecha2 = document.createElement("p");
-                            let splitFecha = item.fechaPre.split(" ");
-                            pIn3Fecha1.innerText = splitFecha[0];
-                            pIn3Fecha2.innerText = splitFecha[1];
-                            divIn3.append(pIn3Fecha1, pIn3Fecha2);
+                            let pIn3Fecha = document.createElement("p");
+                            pIn3Fecha.innerText = item.fechaPre;
+                            divIn3.append(pIn3Fecha);
                             divPrincipal.append(divIn1, divIn2, divIn3);
                             contPre.appendChild(divPrincipal);
                         });
@@ -70,7 +65,6 @@
                 });
     }
     ;
-
     function controlError(err) {
         error.style.display = 'block';
         contPre.style.display = 'none';
@@ -78,11 +72,9 @@
     }
 
     mostrarPreguntas();
-
     function mostrarMensajes() {
 
         let id = $(this).children().eq(0).children().eq(0).text();
-
         const respuestas = async () => {
             try {
                 let idPregunta = new FormData();
@@ -106,6 +98,60 @@
                     } else {
                         console.log(datos);
                         $("#contenedor-preguntas div:odd").remove();
+                        let divPrincipalrespuestas = document.createElement("div");
+                        for (let index = 0; index < datos.length; index++) {
+                            let divRespuesta = document.createElement("div");
+                            let divUsuario = document.createElement("div");
+                            let pUsuario = document.createElement("p");
+                            pUsuario.innerText = datos[index].usuario;
+                            divUsuario.appendChild(pUsuario);
+                            let divFecha = document.createElement("div");
+                            let pFecha = document.createElement("p");
+                            pFecha.innerText = datos[index].fecha;
+                            divFecha.appendChild(pFecha);
+                            let divMensaje = document.createElement("div");
+                            let pMensaje = document.createElement("p");
+                            if (index > 0) {
+                                if (datos[index - 1].secundaria !== null) {
+                                    let pSecundario = document.createElement("p");
+                                    pSecundario.innerText = datos[index - 1].principal;
+                                    divMensaje.appendChild(pSecundario);
+                                }
+                            }
+                            pMensaje.innerText = datos[index].principal;
+                            divMensaje.appendChild(pMensaje);
+                            divRespuesta.append(divUsuario, divFecha, divMensaje);
+                            divPrincipalrespuestas.appendChild(divRespuesta);
+                        }
+                        /*
+                         datos.map((item, index) => {
+                         let divRespuesta = document.createElement("div");
+                         let divUsuario = document.createElement("div");
+                         let pUsuario = document.createElement("p");
+                         pUsuario.innerText = item.usuario;
+                         divUsuario.appendChild(pUsuario);
+                         let divFecha = document.createElement("div");
+                         let pFecha = document.createElement("p");
+                         pFecha.innerText = item.fecha;
+                         divFecha.appendChild(pFecha);
+                         let divMensaje = document.createElement("div");
+                         let pMensaje = document.createElement("p");
+                         console.log(datos[2].secundaria);
+                         if (index !== 0) {
+                         if (datos[--index].secundario !== null) {
+                         let pSecundario = document.createElement("p");
+                         console.log(datos[index].secundaria);
+                         pSecundario.innerText = '"' + datos[index].secundaria + '"';
+                         pMensaje.appendChild(pSecundario);
+                         }
+                         }
+                         pMensaje.innerText = item.principal;
+                         divMensaje.appendChild(pMensaje);
+                         divRespuesta.append(divUsuario, divFecha, divMensaje);
+                         divPrincipalrespuestas.appendChild(divRespuesta);
+                         });
+                         */
+                        $(this).after(divPrincipalrespuestas);
                     }
                 })
                 .catch(error => {
@@ -113,8 +159,7 @@
                     controlError(error);
                 });
     }
-    ;
-        
-    
+
+
 
 </script>
